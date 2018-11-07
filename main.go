653288cd1c6190d03bc4b585d0959aadaa6bd0d3
Steve Birtles <sbirtles@farnborough.ac.uk>
2018-11-07 20:28:33 +0000

    Ready for lesson.

--- main.go
+++ main.go
@@ -39,48 +39,13 @@ var (
 
 func worker(inputs chan coordinate, outputs chan coordinate) {
 
-	for point := range inputs {
 
-		/* --- Calculate value for point --- */
-
-		c := complex(float64(point.x-WIDTH/2)*scale+x, float64(point.y-HEIGHT/2)*scale+y)
-		z := complex(0, 0)
-		var i int
-		for i = 0; i < int(iterations); i++ {
-			z = z*z + c
-			if imag(z) > INFINITY || real(z) > INFINITY {
-				break
-			}
-		}
-		point.value = float64(i) / float64(iterations)
-
-		outputs <- point
-
-	}
 
 }
 
 func resetInputs(inputs chan coordinate) {
 
-	/* --- Clear the input channel --- */
 
-clear:
-	for {
-		select {
-		case <-inputs:
-		default:
-			break clear
-		}
-	}
-
-	/* --- Fill the input channel --- */
-
-	for i := 0; i < WIDTH/2; i++ {
-		for j := 0; j < HEIGHT; j++ {
-			inputs <- coordinate{WIDTH/2 + i, j, 0}
-			inputs <- coordinate{WIDTH/2 - i - 1, j, 0}
-		}
-	}
 
 }
 
@@ -129,19 +94,16 @@ func run() {
 	pic := pixel.PictureDataFromImage(img)
 	sprite := pixel.NewSprite(pic, pic.Bounds())
 
-	/*-----------------------------------------------*/
-
 	/* --- Prepare the input and output channels --- */
 
-	inputs := make(chan coordinate, WIDTH*HEIGHT)
-	outputs := make(chan coordinate, WIDTH*HEIGHT)
-	resetInputs(inputs)
+
+
+	/*-----------------------------------------------*/
+
 
 	/* --- Launch the workers! --- */
 
-	for w := 0; w < THREADS; w++ {
-		go worker(inputs, outputs)
-	}
+
 
 	/*-----------------------------------------------*/
 
@@ -158,94 +120,86 @@ func run() {
 			x = (mouse.X-WIDTH/2)*scale + x
 			y = (HEIGHT/2-mouse.Y)*scale + y
 			scale /= 4
-			resetInputs(inputs)
+			//resetInputs(inputs)
 		}
 		if window.JustPressed(pixelgl.MouseButtonRight) {
 			scale *= 4
-			resetInputs(inputs)
+			//resetInputs(inputs)
 		}
 
 		if window.JustPressed(pixelgl.KeyPageDown) {
 			scale *= 1.25
-			resetInputs(inputs)
+			//resetInputs(inputs)
 		}
 		if window.JustPressed(pixelgl.KeyPageUp) {
 			scale /= 1.25
-			resetInputs(inputs)
+			//resetInputs(inputs)
 		}
 
 		if window.JustPressed(pixelgl.KeyW) {
 			iterations *= 1.25
-			resetInputs(inputs)
+			//resetInputs(inputs)
 		}
 		if window.JustPressed(pixelgl.KeyS) {
 			iterations /= 1.25
-			resetInputs(inputs)
+			//resetInputs(inputs)
 		}
 
 		if window.JustPressed(pixelgl.KeyD) {
 			power *= 1.25
-			resetInputs(inputs)
+			//resetInputs(inputs)
 		}
 		if window.JustPressed(pixelgl.KeyA) {
 			power /= 1.25
-			resetInputs(inputs)
+			//resetInputs(inputs)
 		}
 
 		if window.JustPressed(pixelgl.KeyUp) {
 			y -= (WIDTH / 10) * scale
-			resetInputs(inputs)
+			//resetInputs(inputs)
 		}
 		if window.JustPressed(pixelgl.KeyDown) {
 			y += (WIDTH / 10) * scale
-			resetInputs(inputs)
+			//resetInputs(inputs)
 		}
 		if window.JustPressed(pixelgl.KeyLeft) {
 			x -= (WIDTH / 10) * scale
-			resetInputs(inputs)
+			//resetInputs(inputs)
 		}
 		if window.JustPressed(pixelgl.KeyRight) {
 			x += (WIDTH / 10) * scale
-			resetInputs(inputs)
+			//resetInputs(inputs)
 		}
 
 		if window.JustPressed(pixelgl.Key1) {
 			palette = 0
-			resetInputs(inputs)
+			//resetInputs(inputs)
 		}
 		if window.JustPressed(pixelgl.Key2) {
 			palette = 1
-			resetInputs(inputs)
+			//resetInputs(inputs)
 		}
 		if window.JustPressed(pixelgl.Key3) {
 			palette = 2
-			resetInputs(inputs)
+			//resetInputs(inputs)
 		}
 		if window.JustPressed(pixelgl.Key4) {
 			palette = 3
-			resetInputs(inputs)
+			//resetInputs(inputs)
 		}
 		if window.JustPressed(pixelgl.Key5) {
 			palette = 4
-			resetInputs(inputs)
+			//resetInputs(inputs)
 		}
-
-		/*-----------------------------------------------*/
 
 		select {
 		case <-ticker:
 
 			/* --- Every tick (100ms) drain the output channel and update image --- */
 
-		receiver:
-			for {
-				select {
-				case p := <-outputs:
-					img.Set(p.x, p.y, calculateColor(p.value, palette))
-				default:
-					break receiver
-				}
-			}
+
+
+			/*-----------------------------------------------*/
 
 			pic := pixel.PictureDataFromImage(img)
 			sprite = pixel.NewSprite(pic, pic.Bounds())
